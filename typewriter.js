e3512eac436b3637271be9ee2c8062893bd7ee74
Magi <imytr22@gmail.com>
2026-02-13 22:05:46 -0600

    Improve formatting in typewriter.js
    Refactor spacing and formatting for consistency.

--- typewriter.js
+++ typewriter.js
@@ -1,21 +1,21 @@
-const element = document.getElementById("typewriter");
+const element=document.getElementById("typewriter");
 
-if (element) {
+if(element){
 
-    const text = "Magdalena Yesenia Tristán Rivera | ";
-    let i = 0;
+const text="Magdalena Yesenia Tristán Rivera | ";
+let i=0;
 
-    function typeWriter() {
-        if (i < text.length) {
-            element.innerHTML += text.charAt(i);
-            i++;
-            setTimeout(typeWriter, 70);
-        } else {
-            i = 0;
-            element.innerHTML = "";
-            setTimeout(typeWriter, 500);
-        }
-    }
+function typeWriter(){
+if(i<text.length){
+element.innerHTML+=text.charAt(i);
+i++;
+setTimeout(typeWriter,70);
+}else{
+i=0;
+element.innerHTML="";
+setTimeout(typeWriter,500);
+}
+}
 
-    typeWriter();
+typeWriter();
 }
