a7de6795799e692454f7f16bed815a4f0f322d4b
Magi <imytr22@gmail.com>
2026-02-13 22:42:43 -0600

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,21 +1,19 @@
 const element = document.getElementById("typewriter");
 
 if (element) {
-
     const text = "Magdalena Yesenia Tristán Rivera | ";
     let i = 0;
 
     function typeWriter() {
         if (i < text.length) {
-            element.innerHTML += text.charAt(i);
+            element.innerHTML = text.substring(0, i + 1) + '<span class="cursor">_</span>';
             i++;
-            setTimeout(typeWriter, 70);
+            setTimeout(typeWriter, 100);
         } else {
-            i = 0;
-            element.innerHTML = "";
-            setTimeout(typeWriter, 500);
+            // Elimina el "_" después de terminar si prefieres, 
+            // o déjalo parpadeando con CSS.
+            document.querySelector(".cursor").classList.add("blink");
         }
     }
-
     typeWriter();
 }
